6c3cfb91e49d6b68d0bcbafcfce75b279d5d1559
Kusanemu <134126054+Kusanemu@users.noreply.github.com>
2024-04-24 17:41:50 -0600

    minimenu

---
+++ Minimenu/oal.js
@@ -0,0 +1,26 @@
+function menu(variable){
+
+    switch(variable){
+        case 1:
+            document.getElementById("frame").src="practica1.1.html";
+            break;
+        case 2:
+            document.getElementById("frame").src="practica1.html";
+            break;
+        case 3:
+            document.getElementById("frame").src="practica2.html";
+            break;
+        case 4:
+            document.getElementById("frame").src="practica3.html";
+            break;
+        case 5:
+            document.getElementById("frame").src="reto1.html";
+            break;
+        case 6:
+            document.getElementById("frame").src="arreglos1.html";
+            break;
+        case 7:
+            document.getElementById("frame").src="arreglos2.html";
+            break;
+    }
+}
