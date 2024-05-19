f6c4f3c68a3eb58f19cebce37ce7a37ef5aaa402
Kusanemu <134126054+Kusanemu@users.noreply.github.com>
2024-05-19 15:10:05 -0600

    examen p3

---
+++ examen3/app.js
@@ -0,0 +1,17 @@
+let original=[" ","A","B","C","D","E","F","G","H","I","J","K","L","M","Ñ","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let romano=[" ","I","C","Ñ","H","O","M","T","L","B","S","A","R","Q","P","E","W","J","F","V","U","K","X","D","Y","N","Z","G"];
+
+function encriptar(){
+    let msj=document.getElementById("cadena").value;
+    let texto="";
+    for(let i in msj){
+        for(let j in original){
+            if (msj[i].toUpperCase()==original[j]){
+                texto+=romano[j];
+                break;
+            }
+        }
+    }
+    console.log(texto);
+    document.getElementById("mensaje").innerHTML=texto;
+}
